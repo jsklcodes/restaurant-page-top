7199a6941e15e134f79f13592b9ce867b3e4387d
Kallyne Layane <hellookallyne@gmail.com>
2025-01-25 10:57:28 +0000

    Create home content
    This commit keeps the changes to create the home content, as well style
    it.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,16 @@
+import welcomeImage from '../images/img-1.jpg';
+
 export const getHomePage = () => {
-  return 'Home Page';
+  return `
+    <section class="welcome">
+      <img src="${welcomeImage}" alt="A close-up image of an espresso machine in action, pouring a shot of espresso into a white ceramic cup.">
+      <div>
+        <h2 class="welcome__title">Where Every Sip Tells a Story</h2>
+        <p class="welcome__description">At Mug Shot Cafe, we’re not just about coffee — we’re about connection,
+          community, and crafting the perfect
+          experience for every guest. Whether you’re a coffee connoisseur or just need a cozy spot to unwind, we’ve got
+          something special brewing for you.</p>
+      </div>
+    </section>
+  `;
 };
